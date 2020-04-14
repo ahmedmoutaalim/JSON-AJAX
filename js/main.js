5ec459d6562a1827a70301c1fd629ef043fe78bd
Omar <Omar@omar.com>
2020-04-14 21:35:18 +0100

    add function
    -add Html()
    -ajouté des couleurs

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
+
+
+var button = document.getElementById('zar');
+var info =document.getElementById('info');
+
+button.addEventListener("click",function(){
+
 var xReq = new XMLHttpRequest();
-
 xReq.open('GET','js/cars.json');
-
 xReq.onload = function(){
     var xData = JSON.parse(xReq.responseText);
-  
-
-        document.write(xData[0].name)
-        document.write(xData[0].model)
-        document.write(xData[1].name)
-        document.write(xData[1].model)
+        // document.write(xData[0].name)
+        // document.write(xData[0].model)
+        // document.write(xData[1].name)
+        // document.write(xData[1].model)
+      
+      addHtml(xData); 
     };
-xReq.send();
+    xReq.send();
+
+});
+
+
+function addHtml(data){
+
+    var htmlText = "" ;
+
+    for(i=0; i< data.length; i++){
+
+        htmlText += "<p class=red>" +data[i].name + 
+                    " is a <span class=green>" + data[i].model +"</span><hr></P>" ;
+    }
+    info.insertAdjacentHTML('beforeend',htmlText);
+}
