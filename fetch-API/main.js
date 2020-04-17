3b134ace204bb6f598e0a0097b12492de35cb20d
Omar <Omar@omar.com>
2020-04-17 14:46:14 +0100

    add fetch defi (documentation)

---
+++ fetch-API/main.js
@@ -0,0 +1,35 @@
+const myCatImg = document.getElementById('cat_result');
+const myDogImg = document.getElementById('dog_result');
+
+const catButt = document.getElementById('cat_btn');
+const dogButt = document.getElementById('dog_btn');
+
+catButt.addEventListener('click',getCat);
+dogButt.addEventListener('click',getDog);
+
+function getCat(){
+
+    fetch("https://aws.random.cat/meow")
+    .then(res => res.json())
+    .then(data=>{
+        myCatImg.innerHTML= `<img src="${data.file}"/>`
+    })
+}
+ 
+
+function getDog(){
+
+    fetch("https://random.dog/woof.json")
+    .then(res=>res.json())
+    .then(data=> {
+
+         if(data.url.includes('.mp4')){
+
+            getDog();
+         }else{
+            myDogImg.innerHTML= `<img src="${data.url}"/>`;
+         }
+       
+       
+    })
+}
